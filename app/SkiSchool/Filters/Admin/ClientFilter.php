ecf49db5985df4553a80399fe8bf5d403faed13f
Simon Harvan <harvan.simon@gmail.com>
2022-02-08 22:35:30 +0100

    Add query to client filtering

--- app/SkiSchool/Filters/Admin/ClientFilter.php
+++ app/SkiSchool/Filters/Admin/ClientFilter.php
@@ -16,7 +16,21 @@ class ClientFilter extends Filter
      */
     protected function name($name)
     {
-        return $this->builder->where('name', 'LIKE', '%'. $name . '%');
+        return $this->builder->where('name', 'LIKE', '%' . $name . '%');
+    }
+
+    /**
+     * Filter by clients name or phone or email.
+     * Get all the articles by the user name, phone or email.
+     *
+     * @param $username
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    protected function query($query)
+    {
+        return $this->builder->where('name', 'LIKE', '%' . $query . '%')
+            ->orWhere('phone', 'LIKE', '%' . $query . '%')
+            ->orWhere('email', 'LIKE', '%' . $query . '%');
     }
 
 }
