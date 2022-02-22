93ad85b23e71a104f46df858ff9c406e3882ffdd
Simon Harvan <harvan.simon@gmail.com>
2022-02-22 16:34:16 +0100

    Add configuration for BulkGate

--- app/Console/SmsSender.php
+++ app/Console/SmsSender.php
@@ -3,19 +3,37 @@
 namespace App\Console;
 
 use BulkGate\Message\Connection;
+use BulkGate\Sms\Country;
+use BulkGate\Sms\ISender;
 use BulkGate\Sms\Message as GateMessage;
 use BulkGate\Sms\Sender;
+use BulkGate\Sms\SenderSettings\CountrySenderSettings;
+use BulkGate\Sms\SenderSettings\Gate;
+use BulkGate\Sms\SenderSettings\InvalidGateException;
+use BulkGate\Sms\SenderSettings\StaticSenderSettings;
 
 class SmsSender
 {
 
-    private Sender $sender;
+    const SENDER_NAME = 'Lyž. Medveď';
+    private ISender $sender;
 
     public function __construct()
     {
+
+        try {
+            $settings = new CountrySenderSettings();
+            $settings->add(Country::SLOVAKIA, GATE::GATE6, self::SENDER_NAME)
+                ->add(Country::CZECH_REPUBLIC, GATE::GATE3, self::SENDER_NAME)
+                ->add(Country::POLAND, GATE::GATE3, self::SENDER_NAME);
+        } catch (InvalidGateException $e) {
+            $settings = new StaticSenderSettings(Gate::GATE_TEXT_SENDER, self::SENDER_NAME);
+        }
+
         $connection = new Connection(env('BULK_GATE_APP_ID', 0), env('BULK_GATE_APP_TOKEN', ''));
 
         $this->sender = new Sender($connection);
+        $this->sender->setSenderSettings($settings);
     }
 
     public function sendMessage($phone, $text): bool
